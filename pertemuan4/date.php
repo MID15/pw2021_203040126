eac019c6116f0533d721ed96c3a512c184f362cc
MID15 <mindraw595@gmail.com>
2021-03-01 11:46:55 +0700

    pertemuan 4

--- pertemuan4/date.php
+++ pertemuan4/date.php
@@ -12,22 +12,22 @@
     // Fungsi date/time: time(), date(), mktime(), strtotime()
 
     // Date: untuk menampilkan tanggal dengan format tertentu
-    // echo date("l, d-M-Y");
+    echo date("l, d-M-Y");
 
     // Time
     // UNIX Timestamp / EPOCH time
     // detik yang sudah berlalu sejak 1 januari 1970
-    // echo time();
-    // echo date("l", time()+60*60*24*100);
+    echo time();
+    echo date("l", time()+60*60*24*100);
 
     // mktime
     // membuat sendiri detik yang sudah berlalu
     // mktime(x,x,x,x,x,x)
     // jam, menit, detik, bulan, tanggal, tahun
-    // echo date ("l", mktime(0,0,0,5,15,2002));
+    echo date ("l", mktime(0,0,0,5,15,2002));
 
     // strtotime
-    // echo date ("l", strtotime("25 aug 1985"));
+    echo date ("l", strtotime("25 aug 1985"));
 
     // Fungsi 
         // String: 
