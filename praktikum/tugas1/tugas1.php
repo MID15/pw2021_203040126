dafc04ce5ede9e5f4fb48e6398fc400de9b7e41e
MID15 <mindraw595@gmail.com>
2021-03-04 17:14:37 +0700

    tugas1

--- praktikum/tugas1/tugas1.php
+++ praktikum/tugas1/tugas1.php
@@ -5,50 +5,27 @@
     Shift   : Jum'at 13
     */
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        .container {
-            border: 3px solid black;
-            width: 250px;
-            padding: 2px;
-        }
-        .red {
-            background-color: salmon;
-            height: 30px;
-            width: 30px;
-            margin: 4px;
-            display: inline-block;
-        }
-        .blue {
-            background-color: lightblue;
-            height: 30px;
-            width: 30px;
-            margin: 4px;
-            display: inline-block;
-        }
-    </style>
+    <title>tugas1</title>
 </head>
 <body>
-<div class="container">
-    <?php for ($i = 0; $i < 6; $i++) : ?>
-        <div class="flex">
-        <?php for ($j = 0; $j < 3; $j++) : ?>
-            <?php if ($i %2 == 0) : ?>
-                <div class="red"></div> <div class="blue"></div>
-             <?php else : ?>
-                <div class="blue"></div> <div class="red"></div> 
+<table width="330px" cellspacing="5px" cellpadding="0px" border="1px">
+<?php for( $baris = 1; $baris <= 6; $baris++ ) : ?>
+    <tr>
+        <?php for( $kolom = 1; $kolom <= 6; $kolom++ ) : ?>
+            <?php $i = $baris + $kolom; ?>
+            <?php if( $i % 2 == 1 ) : ?>
+                <td height=50px width=50px bgcolor=salmon></td>
+            <?php else : ?>
+                <td height=50px width=50px bgcolor=lightblue></td>
             <?php endif; ?>
-        <?php endfor; ?>
-        </div>
-        <br>
-    <?php endfor; ?>
-    </div>
+        <?php endfor; ?> 
+    </tr>
+<?php endfor; ?>
 </body>
 </html>
