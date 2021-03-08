fa7586962fc4aa86ce0b8c9a24510d75292431f6
MID15 <mindraw595@gmail.com>
2021-03-08 10:53:31 +0700

    latihan2b

---
+++ praktikum/tugas2/latihan2b.php
@@ -0,0 +1,37 @@
+<?php
+    /*
+    Nama    : Mochamad Indra Wahyudi
+    NRP     : 203040126
+    Shift   : Jum'at 13
+    */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .jawaban {
+            border: 2px solid black;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+<?php 
+    $jawabanIsset = "Isset adalah = Fungsi untuk memeriksa apakah sebuah variable sudah dibuat atau belum <br><br>";
+    $jawabanEmpty = "Empty adalah = Fungsi untuk memeriksa apakah sebuah variable sudah diisikan oleh nilai tertentu";
+
+    function jawaban ($style) {
+        global $jawabanIsset, $jawabanEmpty;
+        echo $jawabanIsset, $jawabanEmpty;
+        return $style;
+    }
+?>
+    <div class="jawaban">
+        <?= jawaban(""); ?>
+    </div>
+</body>
+</html>
