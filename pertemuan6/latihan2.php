d9f31212627067f5f66b04b7c16bb38be0c59580
MID15 <mindraw595@gmail.com>
2021-03-12 09:40:56 +0700

    pertemuan6

--- pertemuan6/latihan2.php
+++ pertemuan6/latihan2.php
@@ -21,15 +21,45 @@ $mahasiswa = [
         "nama" => "Indra Wahyudi", 
     "nrp" => "203040126", 
     "jurusan" => "Teknik Informatika", 
-    "email" => "mindraw595@gmail.com"
+    "email" => "mindraw595@gmail.com",
+    "gambar" => "1.png"
     ],
     [
         "nama" => "Indra Wahyudi", 
     "nrp" => "203040126", 
     "jurusan" => "Teknik Informatika", 
-    "email" => "mindraw595@gmail.com"
+    "email" => "mindraw595@gmail.com",
+    "gambar" => "1.png"
+    ],
+    [
+        "nama" => "Indra Wahyudi", 
+    "nrp" => "203040126", 
+    "jurusan" => "Teknik Informatika", 
+    "email" => "mindraw595@gmail.com",
+    "gambar" => "1.png"
     ]
 ];
-
-echo $mahasiswa[1]["email"];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+        <?php foreach ( $mahasiswa as $m) : ?>
+            <ul>
+                <li>
+                    <img src="img/<?= $m ["gambar"]; ?>" alt="">
+                </li>
+                <li>Nama: <?= $m["nama"] ?></li>
+                <li>NRP: <?= $m["nrp"] ?></li>
+                <li>Jurusan: <?= $m["jurusan"] ?></li>
+                <li>Email: <?= $m["email"] ?></li>
+            </ul>
+        <?php endforeach; ?>
+</body>
+</html>
