cf17dfb71d578a310bca65fdc8c5c86f17bd6d06
MID15 <mindraw595@gmail.com>
2021-03-31 18:38:07 +0700

    latihan3e

--- praktikum/tugas3/latihan3e/index.php
+++ praktikum/tugas3/latihan3e/index.php
@@ -1,87 +1,59 @@
 <?php
-    /*
+/*
     Nama    : Mochamad Indra Wahyudi
     NRP     : 203040126
     Shift   : Jum'at 13
     */
 ?>
-<?php 
-    $items = [
-        [
-            "Picture" => "legion.png",
-            "Name" => "Lenovo Legion 5",
-            "Description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo soluta dolorem recusandae, quisquam nobis molestiae.",
-            "Price" => "Rp 19.500.000",
-            "Category" => "Gaming Laptop"
-        ],
-        [
-            "Picture" => "flow.png",
-            "Name" => "Asus ROG Flow X13",
-            "Description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo soluta dolorem recusandae, quisquam nobis molestiae.",
-            "Price" => "Rp 50.000.000",
-            "Category" => "2-in-1 Gaming Laptop"
-        ],
-        [
-            "Picture" => "zenbook.png",
-            "Name" => "Asus Zenbook 14 UX425",
-            "Description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo soluta dolorem recusandae, quisquam nobis molestiae.",
-            "Price" => "Rp 17.000.000",
-            "Category" => "Ultrabook"
-        ],
-        [
-            "Picture" => "flex.png",
-            "Name" => "Lenovo Ideapad Flex 5",
-            "Description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo soluta dolorem recusandae, quisquam nobis molestiae.",
-            "Price" => "Rp 13.000.000",
-            "Category" => "2-in-1 Laptop"
-        ],
-        [
-            "Picture" => "thinkcentre.jpg",
-            "Name" => "Lenovo AIO PC M802Z",
-            "Description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo soluta dolorem recusandae, quisquam nobis molestiae.",
-            "Price" => "Rp 16.000.000",
-            "Category" => "AIO PC"
-        ],
-        [
-            "Picture" => "legion.png",
-            "Name" => "Lenovo Legion 5",
-            "Description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo soluta dolorem recusandae, quisquam nobis molestiae.",
-            "Price" => "Rp 19.500.000",
-            "Category" => "Gaming Laptop"
-        ],
-        [
-            "Picture" => "flow.png",
-            "Name" => "Asus ROG Flow X13",
-            "Description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo soluta dolorem recusandae, quisquam nobis molestiae.",
-            "Price" => "Rp 50.000.000",
-            "Category" => "2-in-1 Gaming Laptop"
-        ],
-        [
-            "Picture" => "zenbook.png",
-            "Name" => "Asus Zenbook 14 UX425",
-            "Description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo soluta dolorem recusandae, quisquam nobis molestiae.",
-            "Price" => "Rp 17.000.000",
-            "Category" => "Ultrabook"
-        ],
-        [
-            "Picture" => "flex.png",
-            "Name" => "Lenovo Ideapad Flex 5",
-            "Description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo soluta dolorem recusandae, quisquam nobis molestiae.",
-            "Price" => "Rp 13.000.000",
-            "Category" => "2-in-1 Laptop"
-        ],
-        [
-            "Picture" => "thinkcentre.jpg",
-            "Name" => "Lenovo AIO PC M802Z",
-            "Description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo soluta dolorem recusandae, quisquam nobis molestiae.",
-            "Price" => "Rp 16.000.000",
-            "Category" => "AIO PC"
-        ],
+<?php
+$items = [
+    [
+        "gambar" => "hujan.png",
+        "judul" => "Hujan",
+        "penulis" => "Tere Liye",
+        "penerbit" => "Gramedia Pustaka Utama",
+        "tahun" => "Januari 2016",
+        "genre" => "Science-Fiction"
+    ],
+    [
+        "gambar" => "marmut.png",
+        "judul" => "Marmut Merah Jambu",
+        "penulis" => "Raditya Dika",
+        "penerbit" => "Bukune",
+        "tahun" => "Juni 2010",
+        "genre" => "Comedy"
+    ],
+    [
+        "gambar" => "omen.png",
+        "judul" => "Omen",
+        "penulis" => "Lexie Xu",
+        "penerbit" => "Gramedia Pustaka Utama",
+        "tahun" => "September 2012",
+        "genre" => "Thriller, Romance"
+    ],
+    [
+        "gambar" => "bumi.png",
+        "judul" => "Bumi",
+        "penulis" => "Tere Liye",
+        "penerbit" => "Gramedia Pustaka Utama",
+        "tahun" => "Januari 2014",
+        "genre" => "Bildungsroman, Fiksi petualangan, Fantasi"
+    ],
+    [
+        "gambar" => "buma.png",
+        "judul" => "Bumi Manusia",
+        "penulis" => "Pramoedya Ananta Toer",
+        "penerbit" => "Hasta Mitra",
+        "tahun" => "1980",
+        "genre" => "Drama Histori"
     ]
+
+]
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -90,35 +62,42 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <style>
         img {
-            width: 150px;
+            height: 150px;
+        }
+
+        .genre {
+            background-color: red;
         }
     </style>
-        
+
     <title>Hello, world!</title>
-  </head>
-  <body>
+</head>
+
+<body>
     <div class="container">
         <table class="table table-striped table-bordered">
             <thead>
                 <tr>
-                <th scope="col">No</th>
-                <th scope="col">Picture</th>
-                <th scope="col">Name</th>
-                <th scope="col">Description</th>
-                <th scope="col">Price</th>
-                <th scope="col">Category</th>
+                    <th scope="col">No</th>
+                    <th scope="col">Gambar</th>
+                    <th scope="col">Judul</th>
+                    <th scope="col">Penulis/th>
+                    <th scope="col">Penerbit</th>
+                    <th scope="col">Tahun Terbit</th>
+                    <th scope="col">Genre</th>
                 </tr>
             </thead>
             <tbody>
                 <?php $i = 1; ?>
-                <?php foreach ($items as $item): ?>
+                <?php foreach ($items as $item) : ?>
                     <tr>
-                        <th><?= $i; ?></th>
-                        <th><img src="img/<?= $item["Picture"]; ?>" alt=""></th>
-                        <th><?= $item["Name"]; ?></th>
-                        <th><?= $item["Description"]; ?></th>
-                        <th><?= $item["Price"]; ?></th>
-                        <th><?= $item["Category"]; ?></th>
+                        <td><?= $i; ?></td>
+                        <td><img src="img/<?= $item["gambar"]; ?>" alt=""></td>
+                        <td><?= $item["judul"]; ?></td>
+                        <td><?= $item["penulis"]; ?></td>
+                        <td><?= $item["penerbit"]; ?></td>
+                        <td><?= $item["tahun"]; ?></td>
+                        <td class="genre"><?= $item["genre"]; ?></td>
                         <?php $i++; ?>
                     </tr>
                 <?php endforeach; ?>
@@ -136,5 +115,6 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
 
-  </body>
+</body>
+
 </html>
